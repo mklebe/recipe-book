163154615c3326a56b84cc0512b7f7e01d932486
Matthias Klebe <matthiasklebe@gmail.com>
2019-11-18 09:41:22 +0100

    Enhances recipe mock with ingredients

--- src/app/mock-recipe.ts
+++ src/app/mock-recipe.ts
@@ -1,9 +1,42 @@
-import { Recipe } from './recipe';
+import { Recipe } from './model/recipe';
+import { Ingredient } from './model/ingredient';
 
-export const RECIPES: Recipe[] = [
-    new Recipe('Nudelsuppe sauer scharf'),
-    new Recipe('Bockwurst mit Senf'),
-    new Recipe('Chefsalat'),
-    new Recipe('Dreierlei Fleisch'),
-    new Recipe('Moussaka'),
+
+const ingredients = new Map<string, Ingredient>()
+ingredients.set('Bockwurst', new Ingredient('Bockwurst'))
+ingredients.set('Hackfleisch', new Ingredient('Hackfleisch'))
+ingredients.set('Wasser', new Ingredient('Wasser'))
+ingredients.set('Brötchen', new Ingredient('Brötchen'))
+ingredients.set('Senf', new Ingredient('Senf'))
+ingredients.set('Salat', new Ingredient('Salat'))
+ingredients.set('Nuldeln', new Ingredient('Nudeln'))
+ingredients.set('Essig', new Ingredient('Essig'))
+ingredients.set('Chilli', new Ingredient('Chilli'))
+
+export const RECIPE_LIST_MOCK: Recipe[] = [
+    new Recipe('Nudelsuppe sauer scharf', [
+        ingredients.get('Nudeln'),
+        ingredients.get('Wasser'),
+        ingredients.get('Chilli'),
+        ingredients.get('Essig'),
+    ]),
+    new Recipe('Bockwurst mit Senf', [
+        ingredients.get('Bockwurst'),
+        ingredients.get('Senf'),
+        ingredients.get('Wasser'),
+        ingredients.get('Brötchen'),
+    ]),
+    new Recipe('Chefsalat', [
+        ingredients.get('Salat'),
+        ingredients.get('Essig'),
+    ]),
+    new Recipe('Dreierlei Fleisch', [
+        ingredients.get('Wasser'),
+        ingredients.get('Bockwurst'),
+        ingredients.get('Hackfleisch'),
+    ]),
+    new Recipe('Moussaka', [
+        ingredients.get('Wasser'),
+        ingredients.get('Hackfleisch'),
+    ]),
 ]
